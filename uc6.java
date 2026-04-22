0d4a084de3cb482f9a326f968093a1f9e19134e2
daksh23gupta10 <daksh23gupta10@gmail.com>
2026-04-22 15:52:17 +0530

    Create uc6.java

---
+++ uc6.java
@@ -0,0 +1,19 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                names.append(name).append(", ");
+            }
+
+            // Remove trailing ", "
+            String result = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello, " + result + "!");
+        }
+    }
+}
